2a87b966a61235c31e87a13fc9317e4c2bcb2cf1
Hirdesh Kumar <79218874+hirdeshkumar2407@users.noreply.github.com>
2021-12-12 19:36:00 +0500

    ,n

--- server/models/panelistModels/userPanelist.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-const userPanelistSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-});
-
-const userPanelist = mongoose.model("Userpanelist", userPanelistSchema);
-module.exports = userPanelist;
--- server/routes/panelistRoutes/userPanelist.js
+++
@@ -1,6 +0,0 @@
-var express = require("express");
-var router = express.Router();
-var panelistController = require("../../controller/panelistController/userPanelist");
-
-router.post("/login", panelistController.login);
-module.exports = router;
