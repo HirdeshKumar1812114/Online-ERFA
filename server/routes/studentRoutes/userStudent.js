eb00ee8493629537936dd8396a1237e996da0197
Hirdesh Kumar <79218874+hirdeshkumar2407@users.noreply.github.com>
2022-01-27 21:19:11 +0500

    Student verfication not done

--- server/routes/studentRoutes/userStudent.js
+++ server/routes/studentRoutes/userStudent.js
@@ -2,14 +2,14 @@ const jwt = require("jsonwebtoken");
 var express = require("express");
 var router = express.Router();
 var studentController = require("../../controller/studentControlller/userStudent");
-var studentVerificationController = require("../../controller/studentControlller/verStudent");
+
 const {
   requireAuth,
   checkStudent,
   checkUserErfa,
 } = require("../../middleware/authMiddleware");
 
-router.post("/signup", studentVerificationController.checkStudent); //studentController.studentSignUp);
+router.post("/signup");
 router.get("/getallstudents", studentController.getAllStudent);
 router.post("/login", studentController.studentLogin);
 router.post("/dashboard");
