eda679b92102279b9df042ce30ee1654f6ab14ee
Hirdesh Kumar <79218874+hirdeshkumar2407@users.noreply.github.com>
2022-03-02 13:41:07 +0500

    interview  token part 1 done

--- server/controller/erfaController/token.js
+++ server/controller/erfaController/token.js
@@ -2,24 +2,91 @@ const expressAsyncHandler = require("express-async-handler");
 const db = require("../../models");
 
 
-exports.applyToken=expressAsyncHandler(async (req, res)=>{
-    try{
-    const makeToken = new db.Token
-    ({
-        value:req.body.value,
-        time: req.body.time,
-        date: req.body.date,
-        venue: req.body.venue,
-    })
-if(makeToken){
-res.status(200).send(makeToken)
-res.end();
-}else{
-    res.status(400).send({message:'Token not given'})
-}
+exports.applyToken=expressAsyncHandler(async (req,res,next)=>{
 
-    }
-    catch(error){
-        res.status(500).send({message: error.message});
+    console.log(req.body.value);
+    console.log(req.body.time);
+    console.log(req.body.date);
+    console.log(req.body.venue);
+    try{
+  let makeToken = new db.Token({
+      value:req.body.value,
+      time: req.body.time,
+      date: req.body.date,
+      venue: req.body.venue,
+  })
+  
+  const saveToken= await makeToken.save();
+  if(saveToken){
+      res.status(200).send(saveToken);
+      res.end()
+  }
+
+    }catch(err){
+        res.status(500).send({message:err.message})
     }
 })
+
+exports.getAllTokens= expressAsyncHandler(async (req, res, next)=>{
+    try{
+    
+    const fetchAllTokens = await db.Token.find();
+    
+    if(fetchAllTokens){
+        res.status(200).send(fetchAllTokens)
+        res.end();
+    }
+
+
+    }
+    catch(err){
+        res.status(500).send({message:err.message})
+    }
+})
+
+exports.getToken=expressAsyncHandler(async (req, res, next) => {
+    try {
+      const token = await db.Token.findOne({ _id: req.params.id });
+      if (token) {
+        res.status(200).send(token);
+        res.end();
+      } else {
+        res.status(400).json({ message: "Not found" });
+      }
+    } catch (err) {
+      res.status(400).send("Error in delete catch block");
+    }
+  });
+ 
+  
+  exports.removeToken= expressAsyncHandler(async(req, res, next)=>{
+    try{
+    await db.Token.findByIdAndDelete(req.params.id);
+    res.status(200).send({message:'Successfully Delete!'})
+    res.end();
+    }
+    catch(error){
+    res.status(500).send({message:'Error in catch block'})
+    }
+    })
+
+
+    exports.updateToken = expressAsyncHandler(async(req, res, next)=>{
+
+        try{
+        const token = await db.Token.findByIdAndUpdate(
+          req.params.id,
+          { $set: req.body }
+        )
+        
+        if(token){
+          res.status(200).send({message:'Update!'})
+        }else{
+        res.status(400).send({message:'Not Update!'})
+        }
+        }
+        catch(error){
+          res.status(500).send("Error in delete catch block");
+        }
+        
+        })
--- server/routes/erfaRoutes/token.js
+++ server/routes/erfaRoutes/token.js
@@ -4,4 +4,8 @@ var tokenController = require('../../controller/erfaController/token');
 
 
 router.post('/applytoken',tokenController.applyToken)
+router.get('/allTokens',tokenController.getAllTokens)
+router.get('/find/:id',tokenController.getToken)
+router.delete('/delete/:id',tokenController.removeToken)
+router.put('/edit/:id',tokenController.updateToken)
 module.exports = router;
